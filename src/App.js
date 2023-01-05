ea48cf07399d8550a7a4aaa2de1c160f032b95c7
Clay Hamilton <clayham@gmail.com>
2023-01-05 13:20:08 -0500

    added levelcount, username/password check, localStorage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import { useState } from 'react';
 
@@ -6,18 +6,38 @@ import { useState } from 'react';
 const App = () => {
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
+  const [compMessage, setCompMessage] = useState("")
+  const [levelcount, setLevelCount] = useState(0)
 
-  // const submitBad = (e) => {
-  //   e.preventDefault()
-  //   alert("You suck!")
-  // }
+  const levelnames = ["Hello World", "The Beginning", "The End"]
+
+  useEffect(() => { //retrieves username and password from local storage when page opened:
+    const storedUsername = JSON.parse(localStorage.getItem("username"))
+    if (storedUsername) {
+      setUsername(storedUsername)
+    }
+    const storedPassword = JSON.parse(localStorage.getItem("password"))
+    if (storedPassword) {
+      setPassword(storedPassword)
+    }
+  }, [])
+
+  useEffect(() => { //saves username to local storage in case of page refresh
+    localStorage.setItem("username", JSON.stringify(username))
+  }, [username])
+
+  useEffect(() => {
+    localStorage.setItem("password", JSON.stringify(password))
+  }, [password])
+
   const submit = (e) => {
     e.preventDefault()
     if (username == "clayham" && password == "hunter123") {
-      alert("You have logged in successfully!")
+      setCompMessage("You have logged in successfully!")
+      setLevelCount(levelcount+1)
     }
     else {
-      alert("Wrong!")
+      setCompMessage("Wrong!")
     }
     // alert("Username: " + username + " Password: " + password)
     // console.log("Username: " + username + " Password: " + password)
@@ -26,6 +46,17 @@ const App = () => {
   return (
     <div className="App">
       <h1>Passwordle</h1>
+      <h2>Level {levelcount}: {levelnames[levelcount]}</h2>
+      <button onClick={() => setLevelCount(levelcount - 1)}>
+        Previous Level
+      </button>
+      <button onClick={() => setLevelCount(0)}>
+        Reset
+      </button>
+      <button onClick={() => setLevelCount(levelcount + 1)}>
+        Next Level
+      </button>
+      <h3>Please enter your username:</h3>
       <p>Username:</p>
       <form onSubmit={submit}>
         <input value={username} onChange={(e) => setUsername(e.target.value)} />
@@ -34,6 +65,7 @@ const App = () => {
       <form onSubmit={submit}>
         <input value={password} onChange={(e) => setPassword(e.target.value)} />
       </form>
+      <p>{compMessage}</p>
     </div>
   )
 }
