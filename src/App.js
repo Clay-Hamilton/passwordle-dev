6f6c81c0d49b5e8da06a872f525bbd3aa29b1d90
Clay Hamilton <clayham@gmail.com>
2023-01-05 16:25:59 -0500

    minor changes - pre MaterialUI add

--- src/App.js
+++ src/App.js
@@ -65,6 +65,7 @@ const App = () => {
       <form onSubmit={submit}>
         <input value={password} onChange={(e) => setPassword(e.target.value)} />
       </form>
+      <br></br>
       <h3>Computer Voice: {compMessage}</h3>
     </div>
   )
